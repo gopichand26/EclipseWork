4d5cc762c2a0958ec65fd8ef966aa66fcb5517f5
gopic <gopic@192.168.100.6>
2020-08-31 23:58:37 +0530

    Hibernate Annotation

---
+++ HibernateAnnotation/src/main/java/com/dxc/app/App_01.java
@@ -0,0 +1,37 @@
+package com.dxc.app;
+
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
+
+import com.dxc.beans.Student;
+
+public class App_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Configuration configuration = new Configuration().configure();
+		
+		ServiceRegistry serviceRegistry= new ServiceRegistryBuilder().applySettings(configuration.getProperties()).buildServiceRegistry();
+		
+		SessionFactory sessionFactory= configuration.buildSessionFactory(serviceRegistry);
+		Session session = sessionFactory.openSession();
+		Transaction transaction= session.beginTransaction();
+		
+		Student s1 = new Student(102, "gopi", "gopi@gmail.com", "779384");
+		
+		session.save(s1);
+		transaction.commit();
+		System.out.println("Successfully save in DB");
+		
+		session.close();
+		sessionFactory.close();
+
+
+	}
+
+}
